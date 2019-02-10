0a4fc0e005d3e92092ab11bbfbabda92f7fb1915
yu40ta <yu40ta@gmail.com>
2019-02-10 18:22:21 +0900

    #47 Add: doneメソッドの記述、HTML作成メソッドの記述
    - HTMLをメッセージ画面の一番下に追加する記述
    - animateメソッドの記述（メッセージ投稿時に画面を自動下スクロール）
    - 非同期に失敗した場合の処理（alert）の記述

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,15 +1,51 @@
 $(function() {
-  $('.form__message').on('submit', function(e) {
+  function buildHTML(message){
+  var image = `${message.image}` != "null" ?
+  `<img src="${message.image}">` : ``
+  var html  = `<div class="message">
+                 <div class="upper-message">
+                   <div class="upper-message__user-name">
+                     ${message.user_name}
+                   </div>
+                   <div class="upper-message__date">
+                     ${message.created_at}
+                   </div>
+                 </div>
+                 <div class="lower-message">
+                   <p class="lower-message__content">
+                     ${message.content}
+                   </p>
+                   <div class="lower-message__image">
+                     ${image}
+                   </div>
+                 </div>
+               </div>`
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
-    var href = window.location.href
+    var url = $(this).attr('action')
     $.ajax({
-      url: href,
+      url: url,
       type: "POST",
       data: formData,
       dataType: "json",
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $(".messages").animate({ scrollTop: $(".messages")[0].scrollHeight });
+      $('.form__message').val('');
+      $('.form__submit').prop('disabled', false);
+      $('input[type=file]').val('');
+    })
+    .fail(function(){
+      alert('エラー：メッセージを入力してください');
+      $('.form__submit').prop('disabled', false);
     })
   })
 });
