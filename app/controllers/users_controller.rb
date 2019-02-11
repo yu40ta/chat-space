ceadc1271511217f14a023ace1c2dd5146ab0bda
yu40ta <yu40ta@gmail.com>
2019-02-11 11:49:41 +0900

    #52 Fix: 不要な記述の削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,10 @@ class UsersController < ApplicationController
 
   def index
     @users = User.where('name LIKE(?)', "%#{params[:name]}%").where.not(id: current_user.id)
-    respond_to do |format|ß
+    respond_to do |format|
       format.html
       format.json
+    end
   end
 
 
