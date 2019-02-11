6e9bcf27289896657fa1dc27694f7a2cad3bbd01
yu40ta <yu40ta@gmail.com>
2019-02-11 17:23:32 +0900

    #62 Add: 自動更新機能の実装
    #1 turbolinks:loadの追記
    #3-#41 ダブルクォーテーションをシングルクォーテーションに統一
    #51-#78 自動更新機能の記述

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,21 +1,21 @@
-$(function() {
+$(document).on('turbolinks:load',function(){
   function buildHTML(message){
-  var image = `${message.image}` != "null" ?
-  `<img src="${message.image}">` : ``
-  var html  = `<div class="message">
-                 <div class="upper-message">
-                   <div class="upper-message__user-name">
+  var image = `${message.image}` != 'null' ?
+  `<img src='${message.image}'>` : ``
+  var html  = `<div class='message'>
+                 <div class='upper-message'>
+                   <div class='upper-message__user-name'>
                      ${message.user_name}
                    </div>
-                   <div class="upper-message__date">
+                   <div class='upper-message__date'>
                      ${message.created_at}
                    </div>
                  </div>
-                 <div class="lower-message">
-                   <p class="lower-message__content">
+                 <div class='lower-message'>
+                   <p class='lower-message__content'>
                      ${message.content}
                    </p>
-                   <div class="lower-message__image">
+                   <div class='lower-message__image'>
                      ${image}
                    </div>
                  </div>
@@ -29,16 +29,16 @@ $(function() {
     var url = $(this).attr('action')
     $.ajax({
       url: url,
-      type: "POST",
+      type: 'POST',
       data: formData,
-      dataType: "json",
+      dataType: 'json',
       processData: false,
       contentType: false
     })
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $(".messages").animate({ scrollTop: $(".messages")[0].scrollHeight });
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
       $('.form__message').val('');
       $('.form__submit').prop('disabled', false);
       $('input[type=file]').val('');
@@ -48,4 +48,32 @@ $(function() {
       $('.form__submit').prop('disabled', false);
     })
   })
+
+  var interval = setInterval(update, 5000);
+  function update(){
+    if($('.messages')[0]){
+      var message_id = $('.messages').last().data('id');
+    }
+    else {
+      var message_id = 0
+    }
+    $.ajax({
+      url: location.href,
+      type: 'GET',
+      data: { id: message_id },
+      dataType: 'json'
+    })
+    .done(function(data){
+      console.count(setInterval);
+      console.log(data);
+      data.forEach(function(message){
+        var html = buildHTML(message);
+        $('.messages').append(html);
+      })
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
+    })
+    .fail(function() {
+      alert('自動更新に失敗しました');
+    });
+  }
 });
