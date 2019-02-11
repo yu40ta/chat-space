4b89f4925cc59436b63dd64ddbe0d30376181911
yu40ta <yu40ta@gmail.com>
2019-02-11 14:04:48 +0900

    #57 Add: turbolinks:loadの追記

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,5 +1,6 @@
-$(function() {
+$(document).on('turbolinks:load',function(){
   var search_list = $('#user-search-result');
+
   function appendUser(user){
     var html = `<div class='chat-group-user clearfix'>
                   <p class='chat-group-user__name'>
@@ -15,7 +16,7 @@ $(function() {
 
   var search_list_add = $("#chat-group-users");
   function buildHTML(id, name) {
-    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${id}'>
                   <input name='group[user_ids][]' type='hidden' value='${id}'>
                   <p class='chat-group-user__name'>${name}</p>
                   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
@@ -48,10 +49,10 @@ $(function() {
     })
   })
 
-  $('#user-search-result').on('click', '.user-search-add', function(e) {
-    var user_id = $(this).data('user-id');
-    var user_name = $(this).data('user-name');
-    buildHTML(user_id, user_name);
+  $('#user-search-result').on('click', '.user-search-add', function() {
+    var id = $(this).data('user-id');
+    var name = $(this).data('user-name');
+    buildHTML(id, name);
     $(this).parent().remove();
   })
 
